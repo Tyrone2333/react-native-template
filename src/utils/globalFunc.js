e095790a845c161ad00cc39c7c47b526ded74b67
en20 <424021424@qq.com>
2019-11-09 20:49:49 +0800

    feat: 增加任意图片生成接口

--- src/utils/globalFunc.js
+++ src/utils/globalFunc.js
@@ -106,7 +106,16 @@ if (identifyDebugDevelopmentEnvironment()) {
 // window.log = log;
 export {log}
 
+// 任意图片 140*140: https://placeimg.com/140/140/any
+// 生成图片 宽*高/背景/前景.png/&text=文本: https://dummyimage.com/600x400/ccc/000.png&text=文本
+
+// 正方形 116*116: https://i.loli.net/2019/10/16/PNlcn1Cat8pXwYZ.jpg
+// 正方形 212*212: https://tse3-mm.cn.bing.net/th?id=OIP.QNGmLsUJkqUpgMbYBMpPqQAAAA&w=212&h=212&c=7&o=5&dpr=1.25&pid=1.7
+// 圆形 118*118: https://i.loli.net/2019/07/17/5d2f08f66f80754766.png
+// 正方形 白底: https://img.yzcdn.cn/vant/apple-2.jpg
+// 1080p: https://cn.bing.com/th?id=OHR.LeavesGoldfish_ZH-CN6109097460_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
+// 默认头像 蜡笔小新
 export const defaultAvatar =
-    'https://i.loli.net/2019/07/17/5d2f08f66f80754766.png'
+    'https://tse3-mm.cn.bing.net/th?id=OIP.QNGmLsUJkqUpgMbYBMpPqQAAAA&w=212&h=212&c=7&o=5&dpr=1.25&pid=1.7'
 global.defaultProductBanner =
     'https://i.loli.net/2019/07/31/5d4148eee243824369.png'
